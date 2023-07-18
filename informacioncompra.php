e9b819598287b4918f3e6d18e3a2110f20e9819b
JohanUruena2023 <128522657+JohanUruena2023@users.noreply.github.com>
2023-07-18 15:36:35 -0500

    Update informacioncompra.php

--- informacioncompra.php
+++ informacioncompra.php
@@ -4,14 +4,12 @@
   $producto2 = $_POST['producto2'];
   $precio2 = $_POST['precio2'];
   $total = $precio1 + $precio2;
-  echo "Producto 1: ".$producto1;
-echo"<br>";
-  echo "El valor del producto es: ".$precio1;
-echo"<br>";
-  echo "Producto 2: ".$producto2;
-echo"<br>";
-  echo "El valor del producto es:".$precio2;
-  echo"<br>";
-  echo "<p>Total a pagar: </p> .$total;
+echo "Producto 1:".$producto1;
+  echo "Precio 1:".$precio1;
+  echo "Producto 2:".$producto2;
+  echo "Precio 2:".$precio2;
+  echo "Total a pagar:".$total;
+
+  
   ?>
 
