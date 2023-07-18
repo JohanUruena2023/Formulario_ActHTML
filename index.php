d11d28c28f752702ed3e9b3d15e925b90068cbb4
JohanUruena2023 <128522657+JohanUruena2023@users.noreply.github.com>
2023-07-18 15:10:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,22 +8,22 @@
   <form name="producto"class="formulario" action="informacion.php" method="POST">
       <tabla class="tabla" border="1">
 <tr>
- <label for="producto1">Producto 1</label>
+ <label for="producto1">producto 1</label>
  <td>   <input type="text" id="producto1" name="producto1"> </td>
 </tr>
         <br>
        <tr>
-    <lable for="precio1">Precio 1:</label>
+    <lable for="precio1">precio 1:</label>
    <td></td> <input type="number" id="precio1" name="precio1" ></td>
         </tr>
     <br><br>
         <tr>
-    <label for="producto2">Producto 2</label>
+    <label for="producto2">producto 2</label>
  <td> <input type="text" id="producto2" name="producto2" class="required"></td>
         </tr>
     <br>
         <tr>
-    <label for="precio2">Precio 2:</label>
+    <label for="precio2">precio 2:</label>
    <td> <input type="number" id="precio2" name="precio2" class="required"></td>
         </tr>
     <br><br>
