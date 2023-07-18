40446a496d7542ffbc652513adef118ef6ab7427
JohanUruena2023 <128522657+JohanUruena2023@users.noreply.github.com>
2023-07-18 15:29:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
       <tabla class="tabla" border="1">
 <tr>
  <label for="producto1">producto 1</label>
- <td>   <input type="text" id="producto1" name="producto1"> </td>
+ <td>   <input type="text" id="producto1" name="producto1"  class="producto1"> </td>
 </tr>
         <br>
        <tr>
     <lable for="precio1">precio 1:</label>
-   <td></td> <input type="number" id="precio1" name="precio1" ></td>
+   <td></td> <input type="number" id="precio1" name="precio1" class="precio1" ></td>
         </tr>
     <br><br>
         <tr>
     <label for="producto2">producto 2</label>
- <td> <input type="text" id="producto2" name="producto2" class="required"></td>
+ <td> <input type="text" id="producto2" name="producto2" class="producto2"></td>
         </tr>
     <br>
         <tr>
     <label for="precio2">precio 2:</label>
-   <td> <input type="number" id="precio2" name="precio2" class="required"></td>
+   <td> <input type="number" id="precio2" name="precio2" class="precio2"></td>
         </tr>
     <br><br>
         
